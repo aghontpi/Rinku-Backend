40dff8bdc0280f1493e5e33f1093f11498fad829
bluepie <iambluepie@gmail.com>
2023-03-13 01:09:58 +0530

    update instructions

--- interfaces/config.php
+++ interfaces/config.php
@@ -12,6 +12,17 @@ interface config{
     const captcha = "enable";
     const secret = "secret_here";
     const domain = "domain_to_verify_captcha";
+
+//     runnign with docker compose example values
+//     const path = ".";
+//     const host = "database";
+//     const database = "backend_db";
+//     const user = "user";
+//     const password = "user";
+//     const captcha = "disable";
+//     const secret = "secret_here";
+//     const domain = "domain_to_verify_captcha";
+
 }
 
 ?>
